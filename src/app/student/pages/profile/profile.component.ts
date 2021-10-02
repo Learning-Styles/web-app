a77d4674e9993270ae46acfe8ebe68826d5f6acc
DESKTOP-JDJJG3F\Cristian <cris10app@gmail.com>
2021-10-02 09:04:03 -0500

    formulario de perfil

--- src/app/student/pages/profile/profile.component.ts
+++ src/app/student/pages/profile/profile.component.ts
@@ -1,15 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import states from "../../../../assets/json/states.json";
+import cities from "../../../../assets/json/cities.json";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ProfileComponent implements OnInit {
+  activeTab = "general";
+  generalFormSubmitted = false;
+  changePasswordFormSubmitted = false;
+  infoFormSubmitted = false;
+  alertVisible = true;
 
-  constructor() { }
+  departamentosAll: any[] = states['states'];
+  departamentosCol: any[] = [];
 
-  ngOnInit(): void {
+  ciudadAll: any[] = cities['cities'];
+  ciudadCol: any[] = [];
+
+  countries = [
+    { value: "Masculino", name: "Masculino" },
+    { value: "Femenino", name: "Femenino" },
+  ];
+
+  generalForm = new FormGroup({
+    name: new FormControl("Hermione Granger", [Validators.required]),
+  });
+
+  changePasswordForm = new FormGroup({
+    oldPassword: new FormControl("", [Validators.required]),
+    newPassword: new FormControl("", [Validators.required]),
+    retypeNewPassword: new FormControl("", [Validators.required]),
+  });
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  setActiveTab(tab) {
+    this.activeTab = tab;
   }
 
+  get gf() {
+    return this.generalForm.controls;
+  }
+
+  get cpf() {
+    return this.changePasswordForm.controls;
+  }
+
+  onGeneralFormSubmit() {
+    this.generalFormSubmitted = true;
+    if (this.generalForm.invalid) {
+      return;
+    }
+  }
+
+  onChangePasswordFormSubmit() {
+    this.changePasswordFormSubmitted = true;
+    if (this.changePasswordForm.invalid) {
+      return;
+    }
+  }
+
+  cuidadFuncion(data: string) {
+    this.ciudadCol = [];
+    console.log(data);
+
+    for (let index = 0; index < this.ciudadAll.length; index++) {
+      if (this.ciudadAll[index].id_state == data) {
+        this.ciudadCol.push(this.ciudadAll[index]);
+      }
+    }
+
+    console.log(this.ciudadCol);
+  }
 }
